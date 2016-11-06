f77d969248cc1306c6ac8a676dd2b642d7d832bf
Skorkmaz88 <skorkmaz88@yahoo.com>
2016-11-06 09:48:35 +0200

    ...

---
+++ app/routes.js
@@ -0,0 +1,22 @@
+module.exports = function(app) {
+  app.get('/', function(req, res) {
+       res.render('index.ejs',  { }); // load the index.ejs file
+   });
+   app.get('/giveCSV', function(req,res)
+ {
+
+ });
+ app.get('/giveTSV', function(req,res))
+ {
+
+
+ });
+
+}
+
+/////////////////////////////////////////
+// Helper functions
+/////////////////////////////////////////
+// add header
+function()
+{}
--- index.js
+++ index.js
@@ -5,9 +5,13 @@ var mysql      = require('mysql');
 
 // Establishing the connection
 // you can save this data in a safe place rather than explicitly type here
+// We use an example database from MySQL developer website,
+// (https://dev.mysql.com/doc/employee/en/)
 var connection = mysql.createConnection({
   host     : 'localhost', // add your url if different, this is for local machine
   user     : 'root',  // your username
   password : '1234', // your password
-  database : 'node_test' // which database you are connecting
+  database : 'employees' // which database you are connecting
 });
+
+connection.connect();
